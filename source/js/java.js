90b19b396d467309ee645a2ea319bb081895c412
Rina <pamelkk@yandex.ru>
2020-09-14 12:42:47 +0300

    Исправила ошибки

--- source/js/java.js
+++ source/js/java.js
@@ -9,37 +9,25 @@ var name = document.querySelector(".fs-name__field");
 var contacts = document.querySelector(".fs-contacts__field");
 var isStorageSupport = true;
 var storage = "";
-var inputElements = document.querySelector(".fs-name__field");
+
+buttonOpen.addEventListener("click", function (evt) {
+  console.log("Открыть меню!");
+  if (!menu.classList.contains("main-nav-active")) {
+    menu.classList.add("main-nav-active");
+    buttonOpen.classList.add("close-menu");
+  }
+});
 
 buttonBefore.addEventListener("click", function (evt) {
   if (!photoBefore.classList.contains("photo-active")) {
-    photoAfter.classList.remove("photo-active");
-  } else {
     photoBefore.classList.add("photo-active");
+    photoAfter.classList.remove("photo-active");
   }
 });
 
 buttonAfter.addEventListener("click", function (evt) {
   if (!photoAfter.classList.contains("photo-active")) {
-    photoBefore.classList.remove("photo-active");
-  } else {
     photoAfter.classList.add("photo-active");
+    photoBefore.classList.remove("photo-active");
   }
 });
-
-buttonOpen.addEventListener("click", function (evt) {
-  console.log("Открыть меню!");
-  if (!menu.classList.contains("main-nav-active")) {
-    menu.classList.remove("main-nav-active");
-    buttonOpen.classList.remove("close-menu");
-  } else {
-    menu.classList.add("main-nav-active");
-    buttonOpen.classList.add("close-menu");}
-});
-
-[].forEach.call(inputElements, function (item) {
-  item.addEventListener('focus', function(){
-    item.classList.toggle('fs-active', true);
-  });
-});
-
